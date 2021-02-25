fac5625673fcf525784d51625cc97a22c5e54684
antoninWD <anto94@live.ca>
2021-02-25 11:17:17 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1f545254-90b6-484d-bcb5-e4c68026e24e';
+export const MODAL_CLOSE_SEC = 1;
